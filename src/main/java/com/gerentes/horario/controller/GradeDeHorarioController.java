b0458e1238a129ad34790bee3a7f26a2c4afae49
EstefiNy <estefani.rodriguez@escola.pr.gov.br>
2024-08-22 12:11:30 -0300

    CRUD criado nas classes professor e grade

--- src/main/java/com/gerentes/horario/controller/GradeDeHorarioController.java
+++ src/main/java/com/gerentes/horario/controller/GradeDeHorarioController.java
@@ -1,7 +1,9 @@
 package com.gerentes.horario.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,28 @@ public class GradeDeHorarioController {
             e.printStackTrace();
             return "Erro ao criar grade de horário.";
         }
+    }
+    @PostMapping(value = "/criar")
+    public String criar(){
+        System.out.println("Chegou aqui a requisição...");
+        return "criado";
+    } 
+
+    @DeleteMapping(value = "/deletar")
+    public String deletar(){
+        System.out.println("Chegou aqui a requisição...");
+        return "deletado";
+    }
+
+    @GetMapping(value = "/consultar")
+    public String consultar(){
+        System.out.println("Chegou aqui a requisição...");
+        return "consultado";
+    }
+
+    @PutMapping(value = "/alterar")
+    public String alterar(){
+        System.out.println("Chegou aqui a requisição...");
+        return "alterado";
     }
 }
